2ab89b25894f261a0fead2bc8d004482cdfc5fe4
Sri Pritika <124505504+sri123916@users.noreply.github.com>
2026-06-14 00:28:39 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,30 +9,47 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+"/customer",
+session({
+secret: "fingerprint_customer",
+resave: true,
+saveUninitialized: true
+})
+);
 
-app.use("/customer/auth", function auth(req,res,next){
-    // Check if user is logged in and has valid access token
-    if (req.session.authorization) {
-        let token = req.session.authorization['accessToken'];
+app.use("/customer/auth/*", function auth(req, res, next) {
 
-        // Verify JWT token
-        jwt.verify(token, "access", (err, user) => {
-            if (!err) {
-                req.user = user;
-                next(); // Proceed to the next middleware
-            } else {
-                return res.status(403).json({ message: "User not authenticated" });
-            }
-        });
-    } else {
-        return res.status(403).json({ message: "User not logged in" });
-    }
+if (!req.session.authorization) {
+return res.status(403).json({
+message: "User not logged in"
 });
- 
-const PORT =5000;
+}
+
+let token = req.session.authorization.accessToken;
+
+jwt.verify(token, "access", (err, decoded) => {
+
+```
+if (err) {
+  return res.status(403).json({
+    message: "User not authenticated"
+  });
+}
+
+req.user = decoded;
+next();
+```
+
+});
+
+});
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => {
+console.log("Server is running");
+});
